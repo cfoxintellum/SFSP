a6be47acbdba722bc2eaa819231d207ad46671c1
Callum Fox <120175125+cfoxintellum@users.noreply.github.com>
2024-07-26 10:49:11 +0100

    Update showpagelwc.js
    added fetching valid contact and notes

--- showpagelwc.js
+++ showpagelwc.js
@@ -2,15 +2,22 @@ import { LightningElement, wire, track } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
 import {CurrentPageReference} from 'lightning/navigation';
 import jsonData from '@salesforce/resourceUrl/showpagejson';
+import findIsValid from '@salesforce/apex/ShowPageController.findContact';
+//import getNotes from '@salesforce/apex/ShowPageController.findNotes';
 
-const FIELDS = ["Case.AccountId"];
+const FIELDS = ["Case.AccountId", "Case.ContactEmail"];
  
-export default class LoadContact extends LightningElement {
+export default class LoadShow extends LightningElement {
   case;
+  validEmail = false;
+  //accountNotes = "";
   accountID;
   @track recidd;
   jsonToShow;
   showPageData;
+
+
+  dataLoaded = false;
 
   connectedCallback() {
     let request = new XMLHttpRequest();
@@ -24,6 +31,8 @@ export default class LoadContact extends LightningElement {
   getStateParameters(currentPageReference) {
       if (currentPageReference) {
           this.recidd = currentPageReference.attributes.recordId;
+      } else {
+        this.recidd = 0;
       }
   }
 
@@ -31,23 +40,62 @@ export default class LoadContact extends LightningElement {
   @wire(getRecord, { recordId: "$recidd", fields: FIELDS })
   wiredRecord({ error, data }) {
     if (error) {
-      let message = "Unknown error";
       this.showPageData = [];
     } else if (data) {
       this.case = data;
       this.accountID = this.case.fields.AccountId.value;
-
+      this.email = this.case.fields.ContactEmail.value;
+      console.log(this.email);
+      
       let arry = [];
       for(var item of this.jsonToShow) {
         if (item.sfID == this.accountID) {
             let obj = {};
             obj.showPageID = item.showPageID;
             obj.name = item.name;
+            obj.cluster = item.cluster;
             obj.showURL = buildURL(item.showPageID,item.cluster);
             arry.push(obj);
         }
     }
     this.showPageData = arry;
+    this.dataLoaded = true;
+
+    if (this.email != null) {
+      findIsValid({ searchKey: this.email })
+      .then((result) => {
+        if (result.length == 1) {
+          this.validEmail = result[0].Support_Contact__c;
+        }
+      })
+      .catch((error) => {
+          console.log('erro status -' + error);
+      });
+    }
+    /*
+    getNotes({ searchKey: this.accountID })
+    .then((result) => {
+      if (result.length == 1) {
+        this.accountNotes = result[0].Notes__c;
+      }
+    })
+    .catch((error) => {
+        console.log('erro status -' + error);
+    });*/
+
+
+
+    }
+  }
+
+
+
+  toggleSection(event) {
+    let currentsection = this.template.querySelector('[data-id="dropdown"]');
+    if (currentsection.className.search('slds-is-open') == -1) {
+      currentsection.className = 'slds-section slds-is-open';
+    } else {
+      currentsection.className = 'slds-section slds-is-close';
     }
   }
 }
